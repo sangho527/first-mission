d3fd49ac992cfb63529789ad9ab36c30f36bae8c
sangho527 <sy980527@naver.com>
2022-12-24 17:30:55 +0900

    [UPDATE] SecurityConfig 적용 221224

--- src/main/java/com/mission/config/SecurityConfig.java
+++ src/main/java/com/mission/config/SecurityConfig.java
@@ -1,4 +1,30 @@
 package com.mission.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable() // 크로스 사이트 기능
+                .cors().and() // 크로스 사이트 도메인이 다를때 허용해준다는 뜻
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt 사용하는 경우 사용
+                .and()
+                //.addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
