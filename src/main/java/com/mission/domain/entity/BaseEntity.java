d570d128e3ec25886e8f62d08104eae8b2ab569c
sangho527 <sy980527@naver.com>
2023-01-05 00:04:34 +0900

    [Feat] BaseEntity 추가 230105

---
+++ src/main/java/com/mission/domain/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.mission.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@MappedSuperclass // BaseEntity를 상속한 엔티티들은 아래 필드들을 컬럼으로 인식함.
+@EntityListeners(AuditingEntityListener.class) // Auditiing(자동으로 값 매핑) 기능 추가
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+}
