e5698177016968dac268ce9eab44d15a49bb7ae8
sangho527 <sy980527@naver.com>
2023-01-05 09:44:35 +0900

    [Feat] Repository 추가 및 수정 230105

---
+++ src/main/java/com/mission/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.mission.repository;
+
+
+import com.mission.domain.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
--- src/main/java/com/mission/repository/PostRepository.java
+++ src/main/java/com/mission/repository/PostRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.awt.print.Pageable;
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
-    Optional<Post> findByTitle(String title);
+    Page<Post> findByUserId(Pageable pageable, Long userId);
 
 }
